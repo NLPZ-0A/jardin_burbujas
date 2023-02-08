09b6b5d8416e7e180e533bf4d7b69cae7887f35b
nicolas <lopez.nicolas.gabriel09@gmail.com>
2023-02-08 17:14:04 -0300

    actualizacion deploy

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const userRouter = require('./routes/userRouter');
 const app  = require('./index');
-const PORT =  PORT || 4000;
+const port =  process.env.PORT || 4000;
 
 //parseo del reques content
 app.use(express.urlencoded({extended :true}))
@@ -13,6 +13,6 @@ app.use('/', userRouter)
 
 
 //servidor de escucha
-app.listen(PORT, () =>{
-    console.log(`Servidor corriendo en el puerto: ${PORT}`);
+app.listen(port, () =>{
+    console.log(`Servidor corriendo en el puerto: ${port}`);
 });
