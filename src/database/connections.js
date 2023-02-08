0e537a4388d65445082283d06e4b4c3740824afe
nicolas <lopez.nicolas.gabriel09@gmail.com>
2023-02-08 17:52:51 -0300

    nueva actualizacion

--- src/database/connections.js
+++ src/database/connections.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 //conexion a la base de datos
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.USER, process.env.PASSWORD,{
         host : process.env.HOST,
+        port : process.env.PORT_DB,
         dialect: 'mysql',
         operatorsAliases: false,
         logging: false,//no ver los msj en consola sql
