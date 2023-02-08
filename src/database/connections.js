8268fc0da20fecab30a67c34e67de2a89139d5d3
nicolas <lopez.nicolas.gabriel09@gmail.com>
2023-02-08 18:51:50 -0300

    nueva actualizacion

--- src/database/connections.js
+++ src/database/connections.js
@@ -1,4 +1,10 @@
 const Sequelize = require('sequelize');
+import mysql2 from 'mysql2';
+
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
+}
+new Sequelize(options)
 
 //conexion a la base de datos
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.USER, process.env.PASSWORD,{
