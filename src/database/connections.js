41444cc6bf0ce3f68c8b653e4dd212fde798efc4
nicolas <lopez.nicolas.gabriel09@gmail.com>
2023-02-15 18:20:50 -0300

    panel de administración

--- src/database/connections.js
+++ src/database/connections.js
@@ -8,20 +8,22 @@ const mysql2 = require('mysql2');
 new Sequelize(options)
 */
 
-//conexion a la base de datos
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.USER, process.env.PASSWORD,{
-        host : process.env.HOST,
-        port : process.env.PORT_DB,
-        dialect: 'mysql',
-        dialectModule : mysql2,
-       /* dialectOptions: {
-          bigNumberStrings: true,
-          ssl: {
-            rejectUnauthorized: true,
-          }
-        },*/
-        operatorsAliases: false,
-        logging: false,//no ver los msj en consola sql
-});
+const configData = {
+  host : process.env.HOST,
+  port : process.env.PORT_DB,
+  dialect: 'mysql',
+  dialectModule : mysql2,
+  operatorsAliases: false,
+  logging: false,//no ver los msj en consola sql
+}
+
+if (process.env.NODE_ENV === 'production') {
+  configData.dialectOptions = {
+    bigNumberStrings: true,
+    ssl: { rejectUnauthorized: true },
+  };
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.USER, process.env.PASSWORD, configData);
 
 module.exports = sequelize;
